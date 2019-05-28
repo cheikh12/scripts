86ec2bd68c5dbe579758b32309dfc730ddc523e0
cheikh12 <35431896+cheikh12@users.noreply.github.com>
2019-05-28 17:43:28 +0200

    Update pushCheck.sh

--- dtg/pushCheck.sh
+++ dtg/pushCheck.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
-cd Compiler404
-git log -5 --author="cheikh12" --name-only > path.txt
-cut -f2 -d/ -s  path.txt | uniq > paquets.txt
+echo Debut
+
+git log -1  --name-only > path.txt
+cut -f1 -d/ -s  path.txt | uniq > paquets.txt
 
 while read p; do
-  echo "le paquet $p"
+    if [ $p != Jenkinsfile ]; then
+        echo "Modification sur le package $p :  $p"
+        echo "############### Clean Rebuild du package $p ######################"
+        R CMD INSTALL --preclean $p > res_build.txt
+        grep -i " ERROR " res_build.txt
+        if [ $? == 0 ];
+        then
+            rm paquets.txt
+            rm res_build.txt
+            echo BUILD FAILED
+            exit 1
+        else
+            echo "BUILD SUCCESS"
+            rm res_build.txt
+        fi
+        echo "############### Checking packages....pour $p ######################"
+        R CMD Check > res_check.txt
+        grep -i " ERROR " res_check.txt
+        if [ $? == 0 ];
+        then
+            rm paquets.txt
+            rm res_checj.txt
+            echo R CMD Check Failed
+            exit 1
+        else
+            echo "R CMD Check succeded"
+            rm res_check.txt
+        fi
+    fi
 done < paquets.txt
+rm paquets.txt
+
+
